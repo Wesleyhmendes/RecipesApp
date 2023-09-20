a834be14d9584c475c618634c0b7b1ec5158bbfd
Kaique Oliveira <kaiquekurosaki@hotmail.com>
2023-09-20 17:23:08 -0300

    req 2 ao 6 adiciona loginPage

---
+++ src/context/userInfoContext.ts
@@ -0,0 +1,15 @@
+// import { createContext } from 'react';
+
+// type UserInfo = {
+//   name: string;
+//   email: string;
+// };
+
+// type UserInfoContextType = {
+//   userInfo: UserInfo | null;
+//   setUserInfo: (userInfo: UserInfo | null) => void;
+// };
+
+// const UserInfoContext = createContext({} as UserInfoContextType);
+
+// export default UserInfoContext;
---
+++ src/context/userInfoProvider.ts
@@ -0,0 +1,18 @@
+// import React, { useContext, useState } from 'react';
+// import UserInfoContext from './userInfoContext';
+
+// type UserInfo = {
+//   name: string;
+//   email: string;
+// };
+
+// type UserInfoProviderProps = {
+//   children: React.ReactNode;
+// };
+
+// export default function UserInfoProvider({ children }: UserInfoProviderProps) {
+//   const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+//   return (
+//    <UserInfo>
+//   );
+// }
