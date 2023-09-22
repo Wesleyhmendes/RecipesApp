c46b96d62e8318467e15dbb627ac3e611e651040
Wesley Mendes <wesleymendes123321@gmail.com>
2023-09-22 11:04:37 -0300

    [Grupo 28] - Wesley - Req 19, 20 e 21 - Concluídas

--- src/pages/Recipes/index.tsx
+++ src/pages/Recipes/index.tsx
@@ -8,16 +8,7 @@ export default function Recipes() {
   const { drinksData } = useContext(DrinksContext);
   const { mealsData } = useContext(MealsContext);
 
-  const navigate = useNavigate();
   const location = useLocation().pathname;
-
-  if (mealsData?.length === 1 && location === '/meals') {
-    navigate(`/meals/${mealsData[0].idMeal}`);
-  }
-
-  if (drinksData?.length === 1 && location === '/drinks') {
-    navigate(`/drinks/${drinksData[0].idDrink}`);
-  }
 
   if (location === '/meals' && !mealsData) {
     window.alert("Sorry, we haven't found any recipes for these filters.");
